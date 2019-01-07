a3c3566a2f7445db491f8a4a77b9fdba492c26a4
MJ Meyer <mjmeyer@infoslips.com>
2019-01-07 05:56:35 +0200

    load and display sample fish.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 class App extends React.Component {
   state = {
     fishes: {},
@@ -13,14 +15,25 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes });
   };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(fish => (
+              <Fish key={fish} details={this.state.fishes[fish]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
---
+++ src/components/Fish.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+  render() {
+    const { name, image, desc, status, price } = this.props.details;
+
+    return (
+      <div className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name} <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button>Add To Cart</button>
+      </div>
+    );
+  }
+}
+
+export default Fish;
